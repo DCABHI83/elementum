f90a406feae4c449f549f3140995f6069fe79d87
DCABHI83 <araj10569@gmail.com>
2026-04-03 18:17:54 +0530

    built faq section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './UI/Header'
 import Hero from './components/Hero'
 import Body from './components/Body'
 import Offer from './components/Offer'
+import Faq from './components/Faq'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
    <Hero/>
    <Body/>
    <Offer/>
+   <Faq/>
    
    </>
   )
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import image1 from '../assets/image1.png'
+import image2 from '../assets/image2.png'
+import image3 from '../assets/image3.png'
+import image4 from '../assets/image4.png'
+import image5 from '../assets/image5.png'
+import image6 from '../assets/image6.png'
+import image7 from '../assets/image7.png'
+import image8 from '../assets/image8.png'
 const Faq = () => {
   return (
-    <div>
+    <div className='main'>
+        <div className='image relative h-[80vh] border '>
+            <img className='w-[8vw] absolute top-2 left-10' src={image1} alt="image1" />
+            <img className='w-[4vw] absolute top-35 ' src={image2} alt="image1" />
+            <img className='w-[10vw] absolute top-50 left-20' src={image3} alt="image1" />
+            <img className='w-[10vw] absolute bottom-0' src={image4} alt="image1" />
+        </div>
+        <div className='text'></div>
+        <div className='image'></div>
       
     </div>
   )
